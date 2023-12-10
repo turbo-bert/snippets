6f707fdbed0a115d179717b4d6bad958e9bcf266
Robert <turbobert-github@mailbox.org>
2023-12-10 19:24:28 +0100

    ...

--- py/timestamp-str/main.py
+++ py/timestamp-str/main.py
@@ -1,1 +1,2 @@
-tstamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H%M%S")
+#tstamp = datetime.datetime.utcnow().strftime("%Y-%m-%dT%H%M%S")
+tstamp = datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%dT%H%M%S")
