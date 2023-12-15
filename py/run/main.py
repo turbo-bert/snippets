868d71eea4f1fd09e669cd3c3a52bba0d49ff873
Robert <turbobert-github@mailbox.org>
2023-12-15 11:34:57 +0100

    ...

--- py/run/main.py
+++ py/run/main.py
@@ -1,3 +1,7 @@
 import subprocess
 
-subprocess.check_output("ls -al", shell=True, universal_newlines=True)
+#stderr not captured
+try:
+    stdout_lines = subprocess.check_output("ls -al; exit 8;", shell=True, universal_newlines=True).replace("\r", "").strip().split("\n")
+except subprocess.CalledProcessError as e:
+    print("exit code = %d" % e.returncode)
