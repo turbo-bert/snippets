9b3d05039891d8e69cf14436be01e273b3a10733
Robert <turbobert-github@mailbox.org>
2023-12-22 21:44:19 +0100

    ...

---
+++ bash/openssl_rsa.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+openssl genrsa 8192 >key_8k
+openssl rsa -in key_8k -pubout >pubout
+
+dd if=/dev/urandom of=./srcdata bs=1M count=1
+openssl dgst -sha256 -sign key_8k -out srcdata.sig srcdata
+openssl dgst -verify pubout -keyform PEM -sha256 -signature srcdata.sig srcdata 1>/dev/null 2>/dev/null
+ec=$?
+
+if [[ $ec = 0 ]]; then
+    echo sig ok
+else
+    echo sig failed
+fi
