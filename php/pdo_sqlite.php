f200f14dee07ccaa2cef8f8823e44a4b935b015a
Robert <turbobert-github@mailbox.org>
2023-12-24 17:39:39 +0100

    ...

---
+++ php/pdo_sqlite.php
@@ -0,0 +1,37 @@
+<?php
+
+
+try {
+    $D = new PDO('sqlite:./bla.sql');
+    echo "ok\n";
+    $D->beginTransaction();
+
+    $D->exec("DROP TABLE IF EXISTS bla;");
+    $D->exec("CREATE TABLE bla(id serial, name TEXT)");
+
+    $S = $D->prepare('INSERT INTO bla(name) VALUES (:name);');
+    $S->bindParam(':name', $p1);
+    $p1 = "bob";
+    $S->execute();
+    $S = null;
+
+    $S = $D->prepare('INSERT INTO bla(name) VALUES (?);');
+    $S->bindValue(1, "hoho");
+    $S->execute();
+    $S = null;
+
+
+
+
+    if ($D->inTransaction()) {
+        $D->commit();
+    }
+
+    $D = null;
+} catch (PDOException $e) {
+    echo "error $e\n";
+    if ($D->inTransaction()) {
+        $D->rollBack();
+    }
+
+}
