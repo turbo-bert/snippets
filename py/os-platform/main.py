99f8d0a2575f1bd0e833e7aac8a2429da992d70c
Robert <turbobert-github@mailbox.org>
2024-03-12 12:43:25 +0100

    ...

--- py/os-platform/main.py
+++ py/os-platform/main.py
@@ -1,3 +1,7 @@
 import sys
 
 print(sys.platform) # ^linux | win32 | darwin
+
+IS_MACOS = "DARWIN" in sys.platform.upper()
+IS_WIN = "WIN32" in sys.platform.upper()
+IS_LINUX = "LINUX" in sys.platform.upper()
