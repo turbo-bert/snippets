a4630d78d5ee7223279297ed047e818189c82fd4
Robert <turbobert-github@mailbox.org>
2024-05-01 18:09:56 +0200

    ...

--- ffmpeg/motion-multi-stream.sh
+++ ffmpeg/motion-multi-stream.sh
@@ -2,3 +2,6 @@
 
 
 ffmpeg -i 'http://192.168.178.22:8081/101/stream' -t 00:00:01 bla.mp4
+
+# just a frame
+#ffmpeg -i 'http://192.168.178.22:8081/101/stream' -t 00:00:02 -vf "select=eq(n\,10)" -vframes 1 bla.jpg
