b570e645837ac7ef7eb0c20897f4f80a1eb7cd71
Robert <turbobert-github@mailbox.org>
2024-05-24 08:44:05 +0200

    ...

--- py/url/aseag.py
+++ py/url/aseag.py
@@ -6,3 +6,4 @@ nach = "NACH"
 
 x = """{document.getElementById("Inputgen_From").value="%s";document.getElementById("Inputgen_To").value="%s";}""" % (von, nach)
 print("javascript:(function()%s)()" % (urllib.parse.quote(x)))
+# works for firefox and chrome
