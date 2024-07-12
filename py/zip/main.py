bdda8865e1d6f9d48ec7458a497debaeeec6c0f9
Robert <turbobert-github@mailbox.org>
2024-07-12 13:24:29 +0200

    ...

--- py/zip/main.py
+++ py/zip/main.py
@@ -1,4 +1,12 @@
 import zipfile
+import io
+
+code_file = ""
+with io.StringIO() as f:
+    f.write("a")
+    f.write("b")
+    code_file = f.getvalue()
 
 with zipfile.ZipFile("test.zip", "w") as z:
-    z.writestr("/tmp/test/test.txt", "ich bin ein\ntest\n".encode())
+    z.writestr("tmp/test.txt", "ich bin ein\ntest\n".encode())
+    z.writestr("tmp/code_file", code_file.encode())
