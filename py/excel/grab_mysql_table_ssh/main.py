ed59d9bf37a53ed2a8d4bb8facd8ad5c9f2b16df
Robert <turbobert-github@mailbox.org>
2024-09-08 15:52:20 +0200

    ...

--- py/excel/grab_mysql_table_ssh/main.py
+++ py/excel/grab_mysql_table_ssh/main.py
@@ -1,15 +1,31 @@
 import subprocess
 import mxee.helper
+import os
 
 # note works with mysql passwords only when they don't neede escaping
 
-h_ssh = ""
-h_actual = "127.0.0.1"
-d = "testdb"
-t = "test_table"
-u = "root"
-pw = "foo123"
-port = "33060"
+
+#h_ssh = ""
+h_ssh = os.getenv('SSH_HOST', '')
+
+#h_actual = "127.0.0.1"
+h_actual = os.getenv('HOST_ACTUAL', "127.0.0.1")
+
+#d = "testdb"
+d = os.getenv('HOST_DB', "testdb")
+
+#t = "test_table"
+t = os.getenv('MYSQL_TABLE', 'test_table')
+
+#u = "root"
+u = os.getenv('MYSQL_USER', 'root')
+
+#pw = "foo123"
+pw = os.getenv('MYSQL_PW', 'foo123')
+
+#port = "33060"
+port = os.getenv('MYSQL_PORT', '33060')
+
 
 my_tempdir = subprocess.check_output("""/bin/bash -c 'mktemp -d /tmp/tabledump-XXXXXXXX'""", shell=True, universal_newlines=True).strip().split("\n")[0].strip()
 
@@ -19,6 +35,17 @@ cmd = """mysql --batch -u %s -p --password=%s -h %s -P %s -e '%s' %s""" % (u, pw
 if h_ssh != "":
     cmd = "ssh %s %s" % (h_ssh, cmd)
 
-subprocess.call("""/bin/bash -c "%s >%s" """ % (cmd, my_tempdir + "/out"), shell=True)
+
+tsvfilename = my_tempdir + "/out"
+subprocess.call("""/bin/bash -c "%s >%s" """ % (cmd, tsvfilename), shell=True)
+
+data = None
+lines = None
+with open(tsvfilename, 'r') as f:
+    lines = f.read().strip().split("\n")
+
+data = [line.strip().split("\t") for line in lines]
 
 subprocess.call("""/bin/bash -c "rm -fr %s" """ % (my_tempdir), shell=True)
+
+mxee.helper.xlsx_out("out.xlsx", {t:{'data':data}})
--- py/excel/grab_mysql_table_ssh/run_createtable.sh
+++ py/excel/grab_mysql_table_ssh/run_createtable.sh
@@ -1,6 +1,13 @@
 #!/bin/bash
 
 
+if [[ ! -d db ]]; then
+
+mkdir db
+
+( cd db && osd mysql )
+
+sleep 15
 
 echo "CREATE DATABASE testdb;" | mysql -u root -p --password=foo123 -h 127.0.0.1 -P 33060
 echo "CREATE TABLE test_table (id INTEGER AUTO_INCREMENT, name VARCHAR(80), PRIMARY KEY(id));" | mysql -u root -p --password=foo123 -h 127.0.0.1 -P 33060 testdb
@@ -8,3 +15,5 @@ echo "CREATE TABLE test_table (id INTEGER AUTO_INCREMENT, name VARCHAR(80), PRIM
 for I in $(seq 1 100); do
     echo "INSERT INTO test_table (name) VALUES ('bla');" | mysql -u root -p --password=foo123 -h 127.0.0.1 -P 33060 testdb
 done
+
+fi
