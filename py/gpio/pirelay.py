840b1ce413c515884e6ef4993c78a5e6da6b575b
Robert <turbobert-github@mailbox.org>
2024-10-07 22:53:01 +0200

    ...

--- py/gpio/pirelay.py
+++ py/gpio/pirelay.py
@@ -4,6 +4,12 @@
 import time
 import gpiozero
 
+# newest:https://shop.sb-components.co.uk/products/pirelay-relay-board-shield-for-raspberry-pi
+# rel1=gpio19
+# rel2=gpio13
+# rel3=gpio6
+# rel4=gpio5
+
 # 4 17 27 22
 od = gpiozero.OutputDevice(22, active_high=True, initial_value=False)
 od.on()
