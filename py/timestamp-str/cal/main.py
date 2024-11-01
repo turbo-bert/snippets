aadf3e92d29d7ca2f445fda24156d80244be7b54
Robert <turbobert-github@mailbox.org>
2024-11-01 20:14:30 +0100

    ...

--- py/timestamp-str/cal/main.py
+++ py/timestamp-str/cal/main.py
@@ -3,7 +3,13 @@ import datetime
 
 too_day = datetime.datetime.now(datetime.UTC)
 
+#or from string YYYY-MM-DD
+too_day = datetime.datetime.fromisoformat("2012-08-09T00:00:00+0000")
+
+
 some_days = [ too_day - datetime.timedelta(days=x) for x in range(7)]
+
+print(some_days)
 
 for x in some_days:
     print(x.strftime("%Y-%m-%d") + x.strftime(" %a"))
