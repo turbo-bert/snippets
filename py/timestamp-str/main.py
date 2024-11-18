2369f2174301938e2049e08cf4f6f89f8593a2a8
Robert <turbobert-github@mailbox.org>
2024-11-18 20:05:41 +0100

    ...

--- py/timestamp-str/main.py
+++ py/timestamp-str/main.py
@@ -2,3 +2,14 @@
 import datetime
 tstamp = datetime.datetime.now(datetime.UTC).strftime("%Y-%m-%dT%H%M%S")
 print(tstamp)
+
+
+
+t1 = datetime.datetime.fromisoformat("2012-08-09T00:00:00+00:00")
+t2 = t1 + datetime.timedelta(minutes=60)
+
+print(t1.isoformat())
+print(t2.isoformat())
+
+print(int(t1.timestamp()))
+print(int(t2.timestamp()))
