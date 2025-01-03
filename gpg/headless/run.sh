0390224563b268bba03ce3aad735dadf1a9337e8
Robert <turbobert-github@mailbox.org>
2025-01-03 21:23:27 +0100

    ...

--- gpg/headless/run.sh
+++ gpg/headless/run.sh
@@ -1,6 +1,10 @@
 #!/bin/bash
 
 
+
+NAME="Bob"
+ADDR="sinclair@turbobert.de"
+
 rm -fr ghome
 mkdir ghome
 GH="`pwd`/ghome"
@@ -15,8 +19,8 @@ Key-Type: default
 Key-Curve: Ed25519
 Subkey-Type: default
 Subkey-Curve: Ed25519
-Name-Real: test
-Name-Email: test@turbobert.de
+Name-Real: $NAME
+Name-Email: $ADDR
 Expire-Date: 0
 %no-protection
 %commit
@@ -24,3 +28,8 @@ EOF
 
 #Key-Length: 2048
 #Subkey-Length: 2048
+
+gpg --homedir "$GH" --batch --no-tty --export-secret-keys -a >export_secret.asc
+gpg --homedir "$GH" --batch --no-tty --export -a >export_public.asc
+gpg --homedir "$GH" --batch --no-tty --list-keys --fingerprint >export_fingerprint.asc
+gpg --homedir "$GH" --batch --no-tty --list-keys --fingerprint | grep "^   " | head -1 | tr -d ' ' | tr -d '\n' >export_fingerprint_only.asc
--- gpg/headless/run_clean.sh
+++ gpg/headless/run_clean.sh
@@ -2,3 +2,5 @@
 
 
 rm -fr ghome
+rm -f *.asc
+
