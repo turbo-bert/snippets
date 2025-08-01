a30917ea13965341f5f5d44b266407418f241e4b
Robert <turbobert-github@mailbox.org>
2025-08-01 22:04:51 +0200

    ...

--- py/np/plot/main.py
+++ py/np/plot/main.py
@@ -1,15 +1,22 @@
 import numpy as np
 import matplotlib.pyplot as plt
+import math
 
-x = np.linspace(-2,2,100)
-y = x**2
+x = np.linspace(0,1,250)
+
+t=0.12
+f = lambda x: 5*t*(0.2969*np.sqrt(x) - 0.1260 * x - 0.3516*x**2 + 0.2843*x**3-0.1015*x**4)
+y = f(x)
+
+
 
 plt.plot(x,y, color="black")
+plt.plot(x,(-1)*y, color="black")
 
 plt.xlabel("X")
 plt.ylabel("Y")
 plt.title("T")
-
+plt.axis('equal')
 plt.grid(True)
 plt.savefig("test.svg", bbox_inches="tight")
 plt.savefig("test.jpg", bbox_inches="tight", dpi=300)
