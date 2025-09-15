13d6fc835890831c5a5cafa178d6b4e40084aa13
Robert <turbobert-github@mailbox.org>
2025-09-15 07:48:35 +0200

    ...

--- py/pi/relay/main.py
+++ py/pi/relay/main.py
@@ -1,5 +1,6 @@
 import time
 import gpiozero
+#from apt install python3-gpiozero
 
 # 4 17 27 22                                                                                                      
 od = gpiozero.OutputDevice(5, active_high=True, initial_value=False)
